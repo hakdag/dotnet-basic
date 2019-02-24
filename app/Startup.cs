9f8c84924cc4e853e6c277eb7e3c071a15e92ddf
hakdag <hakanakdag46@gmail.com>
2019-02-24 17:23:53 +0300

    test

--- app/Startup.cs
+++ app/Startup.cs
@@ -27,7 +27,7 @@ namespace web1
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World! Feature7 added...");
+                await context.Response.WriteAsync("Hello World! Feature7 added..");
             });
         }
     }
