9227a3e8c5d5d6e9e284086de96000737ab3024f
hakdag <hakanakdag46@gmail.com>
2019-02-24 16:28:19 +0300

    docker changes.

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,3 +1,7 @@
-docker image rm dotnet-basic --force
+#!/bin/bash
+
+IMAGE_TAG=$1
+
+docker image rm $IMAGE_TAG --force
 docker ps -aq --no-trunc -f status=exited | xargs docker rm
-docker build --tag=dotnet-basic .
+docker build --tag=$IMAGE_TAG .
