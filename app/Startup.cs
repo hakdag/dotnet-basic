89e5550673dc71efab634c58a56e36ba054f135d
hakdag <hakanakdag46@gmail.com>
2019-03-01 11:46:35 +0300

    Feature11 added.

--- app/Startup.cs
+++ app/Startup.cs
@@ -27,7 +27,7 @@ namespace web1
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World! Feature10 added...");
+                await context.Response.WriteAsync("Hello World! Feature11 added...");
             });
         }
     }
